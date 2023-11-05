63c408df8e96060f5f00de3a8a6cbc734148c7eb
Emanuelle Sales <39285319+EmanuelleSalesG@users.noreply.github.com>
2023-11-05 16:36:23 -0300

    adding checkbox button in the tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,15 @@ const Page = () => {
     setList(list.filter((item, key) => key !== index));
   }
 
+  const toggleItem = (index: number) => {
+      let newList = [...list];
+      newList[index].checked = !newList[index].checked;
+      setList(newList);
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col items-center text-2xl bg-black text-white">
-      <h1 className="text-4xl mt-5 font-semibold">Todo ✍🏻</h1>
+      <h1 className="text-4xl mt-5 font-semibold font-mono">Todo ✍🏻</h1>
 
       <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-blue-700 border-2 border-blue-800">
           <input type="text" 
@@ -46,7 +52,11 @@ const Page = () => {
       <ul className="w-full max-w-lg pl-5">
           {list.map((item, index) => (
             <>
-              <li key={index} className="p-2 flex justify-between">{item.label}       
+              <li key={index} className="p-2 flex justify-between">
+              <div>
+                <input onClick={() => toggleItem(index)} className="mr-2 w-5 h-5" type="checkbox" checked={item.checked} />  
+                {item.label}
+              </div>       
               <button onClick={() => deleteItem(index)} className="bg-blue-800 ml-2  rounded-full px-2 text-xl mr-2 hover:opacity-80">Close</button> 
               </li>
               <hr />
