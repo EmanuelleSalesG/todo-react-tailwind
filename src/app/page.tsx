6aa2564a97c3c7f2fb944103292e3d58aef75fc5
Emanuelle Sales <39285319+EmanuelleSalesG@users.noreply.github.com>
2023-11-05 15:34:58 -0300

    adding items to the list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 "use client"
 
+import { TodoItem } from "@/types/TodoItem";
 import { useState } from "react";
 
 const Page = () => {
-  const [list, setList] = useState([]);
+
+  const [itemInput, setItemInput] = useState('');
+
+  const [list, setList] = useState<TodoItem[]>([
+    {label: 'Test', checked: false},
+    {label: 'Test', checked: true}
+  ]);
+
+  const handleAddButton = () => {
+    setList([
+      ...list, {label: itemInput, checked: false}
+    ]);
+    setItemInput('');
+  }
 
   return (
     <div className="w-screen h-screen flex flex-col items-center text-2xl bg-black text-white">
@@ -12,15 +26,26 @@ const Page = () => {
       <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-blue-700 border-2 border-blue-800">
           <input type="text" 
           placeholder="What do you want to do? "
-          className="flex-1 bg-blue-700 outline-none text-2xl mr-3"/>
-          <button className="text-4xl">+</button>
+          className="flex-1 bg-blue-700 outline-none text-2xl mr-3"
+          value={itemInput}
+          onChange={e => setItemInput(e.target.value)}/>
+          <button onClick={handleAddButton} className="text-4xl">+</button>
+      </div>
+
+      <div>
+        <p className="border border-white p-2 rounded-md m-2">{list.length} tasks listed</p>
       </div>
 
       <ul className="w-full max-w-lg pl-5">
-          <li className="p-2 flex justify-between">Task 
-          <button className="bg-blue-800  rounded-full px-2 text-xl mr-2 hover:opacity-80">Close</button>
-          </li>
-          <hr />   
+          {list.map(item => (
+            <>
+              <li className="p-2 flex justify-between">{item.label} 
+              <button className="bg-blue-800  rounded-full px-2 text-xl mr-2 hover:opacity-80">Close</button>
+              </li>
+              <hr />
+            </> 
+
+          ))}  
       </ul>
     </div>
   );
